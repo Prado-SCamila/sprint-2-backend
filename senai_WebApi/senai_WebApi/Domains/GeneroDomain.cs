8b7d6a2654750610e56399c8745421fe444b89d6
prado <camila.silva54@senaisp.edu.br>
2021-03-23 21:10:25 -0300

    Domains adicionadas

---
+++ senai_WebApi/senai_WebApi/Domains/GeneroDomain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_WebApi.Domains
+{
+    public class GeneroDomain
+    {
+        public int idGenero{ get; set; }
+
+        public string nome { get; set; }
+    }
+}
